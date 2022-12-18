424a803ff75b9a38236cd70ded35b434d5db7014
Rafiw Idrissu <86615946+Idrissu-Rafiu@users.noreply.github.com>
2022-12-17 16:39:01 -0800

    add title and instructions

--- src/App.jsx
+++ src/App.jsx
@@ -51,12 +51,16 @@ function App() {
 
     if (result) {
       setTenzies(true);
-      console.log("tenzies");
     }
   }, [dice]);
 
   return (
     <main>
+      <h2 className="title">Tenzies</h2>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       {tenzies && <Confetti />}
       <div className="dice-container">
         {dice.map((die) => (
